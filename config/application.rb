f6edbeb4b35b7bb812bf30850c1793166918e3b1
samuel <samuel.whitley89@gmail.com>
2015-04-27 16:50:47 -0400

    precompile

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Webpage
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('/app/assets/fonts')
+    config.assets.initialize_on_precompile = false
   end
 end
