0625486a7948ea2522e7ecaef5ad46ab6b2238e1
samuel <samuel.whitley89@gmail.com>
2015-04-27 17:18:22 -0400

    precompile issues

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Webpage
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.initialize_on_precompile = false
+    
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('/app/assets/fonts')
